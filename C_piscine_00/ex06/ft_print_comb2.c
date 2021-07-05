a408611231e96231597190f6c2930166ea490975
António Abranches Pinto <71015248+20AAP02@users.noreply.github.com>
2021-07-05 19:18:15 +0100

    Add files via upload

---
+++ C_piscine_00/ex06/ft_print_comb2.c
@@ -0,0 +1,83 @@
+#include <unistd.h>
+
+void	write_1(int x, int y)
+{
+	char	a;
+	char	b;
+
+	a = x + '0';
+	b = y + '0';
+	write(1, "0", 1);
+	write(1, &a, 1);
+	write(1, " ", 1);
+	write(1, "0", 1);
+	write(1, &b, 1);
+	write(1, ",", 1);
+	write(1, " ", 1);
+}
+
+void	write_2(int x, int y)
+{
+	char	a;
+	char	y1;
+	char	y2;
+
+	a = x + '0';
+	y1 = (y / 10) + '0';
+	y2 = (y % 10) + '0';
+	write(1, "0", 1);
+	write(1, &a, 1);
+	write(1, " ", 1);
+	write(1, &y1, 1);
+	write(1, &y2, 1);
+	write(1, ",", 1);
+	write(1, " ", 1);
+}
+
+void	write_3(int x, int y)
+{
+	char	x1;
+	char	x2;
+	char	y1;
+	char	y2;
+
+	x1 = (x / 10) + '0';
+	x2 = (x % 10) + '0';
+	y1 = (y / 10) + '0';
+	y2 = (y % 10) + '0';
+	write(1, &x1, 1);
+	write(1, &x2, 1);
+	write(1, " ", 1);
+	write(1, &y1, 1);
+	write(1, &y2, 1);
+	if (x != 98 || y != 99)
+	{
+		write(1, ",", 1);
+		write(1, " ", 1);
+	}
+	else
+		write(1, "\n", 1);
+}
+
+void	ft_print_comb2(void)
+{
+	int	x;
+	int	y;
+
+	x = 0;
+	while (x < 100)
+	{
+		y = 0;
+		while (y < 100)
+		{
+			if (x < y && x < 10 && y < 10)
+				write_1(x, y);
+			else if (x < y && x < 10 && y > 9)
+				write_2(x, y);
+			else if (x < y && x > 9 && y > 9)
+				write_3(x, y);
+			y++;
+		}
+		x++;
+	}
+}
