c85e107c5b583de48082829b4ff6d3fb417de23f
António Abranches Pinto <71015248+20AAP02@users.noreply.github.com>
2021-07-06 22:15:18 +0100

    Update ft_strcapitalize.c

--- c02/ex09/ft_strcapitalize.c
+++ c02/ex09/ft_strcapitalize.c
@@ -1,5 +1,3 @@
-#include <stdio.h>
-
 int check_if_letter(char a)
 {
     if (a < 65 || a > 90 && a < 97 || a > 122)
@@ -12,24 +10,17 @@ int check_if_letter(char a)
 char *ft_strcapitalize(char *str)
 {
     int s = 0;
-    while(str[s] != '\0')
+    while (str[s] != '\0')
     {
         if (str[s] < 123 && str[s] > 96)
         {
-            if (s == 0 || check_if_letter(str[s-1]) == 1)
+            if (s == 0 || check_if_letter(str[s - 1]) == 1)
             {
                 str[s] = str[s] - 32;
             }
         }
-        printf("%c", str[s]);
         s++;
     }
 
     return str;
-}
-
-int main()
-{
-    char *ft = "oi, tudo bem? 42palavras quarenta-e-duas; cinquenta+e+um";
-    printf("%s", ft_strcapitalize(ft));
 }
