738a7c21676318d068686276517492e719e5f052
António Abranches Pinto <71015248+20AAP02@users.noreply.github.com>
2021-07-07 19:13:25 +0100

    Delete Shell_01 directory

--- Shell_01/ex07/r_dwssap.sh
+++
@@ -1,1 +0,0 @@
-cat /etc/passwd | awk 'NR > 10' | sed -n 'n;p' | cut -d: -f1 | sort -r | awk 'NR>= ENVIRON["FT_LINE1"] && NR<= ENVIRON["FT_LINE2"]' | paste -s -d"," - | sed 's/,/, /g' | sed 's/$/./' | tr -d '\n'
