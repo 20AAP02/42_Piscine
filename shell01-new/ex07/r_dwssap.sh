efbf9d80b161c9b3b20c5ad9a098cffbdae70ed6
António Abranches Pinto <71015248+20AAP02@users.noreply.github.com>
2021-07-07 19:13:47 +0100

    Delete shell01-new directory

--- shell01-new/ex07/r_dwssap.sh
+++
@@ -1,1 +0,0 @@
-cat /etc/passwd | sed '/^#/d' | sed -n 'n;p' | cut -d ':' -f1 | rev | sort -r | awk 'NR>= ENVIRON["FT_LINE1"] && NR<= ENVIRON["FT_LINE2"]' | paste -sd ',' - | sed 's/,/,/g' | sed 's/$/./' | tr -d '\n'
