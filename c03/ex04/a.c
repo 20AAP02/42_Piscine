1dee7298839661834746babae8f8f26533a4241b
António Abranches Pinto <71015248+20AAP02@users.noreply.github.com>
2021-07-10 19:54:01 +0100

    Create a.c

---
+++ c03/ex04/a.c
@@ -0,0 +1,17 @@
+int		ft_str_is_lowercase(char *str)
+{
+	int is_lowercase;
+	int i;
+
+	is_lowercase = 1;
+	i = 0;
+	while (str[i] != '\0')
+	{
+		if (!(str[i] >= 'a' && str[i] <= 'z'))
+		{
+			is_lowercase = 0;
+		}
+		i++;
+	}
+	return (is_lowercase);
+}
